f485462458ae47788f875709ec15caba5a68bd68
mikoloism <mikoloism@gmail.com>
2023-03-19 21:53:12 +0330

    feat(shape): re-export shapes from `src/shape/`;
    add `shape` type to work in outside of the `shape/`;

---
+++ src/shape/index.ts
@@ -0,0 +1,6 @@
+export { default as Circle } from './circle';
+export { default as Position } from './position';
+export { default as Rectangle } from './rectangle';
+export { default as Square } from './square';
+export { default as Triangle } from './triangle';
+export * from './type';
---
+++ src/shape/type.ts
@@ -0,0 +1,6 @@
+import Rectangle from './rectangle';
+import Square from './square';
+import Triangle from './triangle';
+import Circle from './circle';
+
+export type Shape = Rectangle | Square | Triangle | Circle;
