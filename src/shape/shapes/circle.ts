058a1dd3fd17e492728f96a37f84e13665976442
mikoloism <mikoloism@gmail.com>
2023-03-20 03:13:49 +0330

    fix(shape): udpate `circle` and `square` draw method to use position dynamic size

--- src/shape/shapes/circle.ts
+++ src/shape/shapes/circle.ts
@@ -1,21 +1,33 @@
 import BaseShape, { ShapeKind } from '../helpers/base';
 import Color from '../helpers/color';
-import Position from '../helpers/position';
+import Position, { type PointOfGravity } from '../helpers/position';
 
 export default class Circle extends BaseShape {
 	public type: ShapeKind = ShapeKind.CIRCLE;
+	public size: number = 50;
+	public pointOfGravity: PointOfGravity = 'center';
 
 	public constructor(public position: Position) {
 		super();
 	}
 
 	public draw($: CanvasRenderingContext2D): void {
+		this.position.calcPoint(this.pointOfGravity, this.size);
 		const startPoint = this.position.startPoint;
 		const color = Color.createSchema(this.color);
-		const radius = 50;
+		const startArc = 0;
+		const endArc = /* 360deg */ 2 * Math.PI;
+		const isClockwise = false;
 
 		let shape = new Path2D();
-		shape.arc(startPoint.x, startPoint.y, radius, 0, 2 * Math.PI, false);
+		shape.arc(
+			startPoint.x,
+			startPoint.y,
+			this.size,
+			startArc,
+			endArc,
+			isClockwise,
+		);
 		$.fillStyle = color.fill;
 		$.fill(shape);
 		$.lineWidth = 10;
--- src/shape/shapes/square.ts
+++ src/shape/shapes/square.ts
@@ -1,20 +1,23 @@
 import BaseShape, { ShapeKind } from '../helpers/base';
 import Color from '../helpers/color';
-import Position from '../helpers/position';
+import Position, { type PointOfGravity } from '../helpers/position';
 
 export default class Square extends BaseShape {
 	public type: ShapeKind = ShapeKind.SQUARE;
+	public size: number = 100;
+	public pointOfGravity: PointOfGravity = 'center';
 
 	public constructor(public position: Position) {
 		super();
 	}
 
 	public draw($: CanvasRenderingContext2D): void {
+		this.position.calcPoint(this.pointOfGravity, this.size);
 		const startPoint = this.position.startPoint;
 		const color = Color.createSchema(this.color);
 
 		let shape = new Path2D();
-		shape.rect(startPoint.x, startPoint.y, 100, 100);
+		shape.rect(startPoint.x, startPoint.y, this.size, this.size);
 		$.fillStyle = color.fill;
 		$.fill(shape);
 		$.lineWidth = 10;
