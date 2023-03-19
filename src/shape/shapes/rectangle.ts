a2addbe0ae059c0980c2b5885464f46cae852980
mikoloism <mikoloism@gmail.com>
2023-03-20 03:13:01 +0330

    fix(shape): add `rectangle` to shapes (update draw method);

--- src/shape/shapes/rectangle.ts
+++ src/shape/shapes/rectangle.ts
@@ -1,15 +1,28 @@
 import BaseShape, { ShapeKind } from '../helpers/base';
-import Position from '../helpers/position';
+import Color from '../helpers/color';
+import Position, { type PointOfGravity } from '../helpers/position';
 
 export default class Rectangle extends BaseShape {
 	public type: ShapeKind = ShapeKind.RECTANGLE;
+	public width: number = 100;
+	public height: number = 100;
+	public pointOfGravity: PointOfGravity = 'center';
 
 	public constructor(public position: Position) {
 		super();
 	}
 
 	public draw($: CanvasRenderingContext2D): void {
-		$.beginPath();
-		$.fillRect(this.position.x, this.position.y, 100, 100);
+		this.position.calcPoint(this.pointOfGravity, this.width, this.height);
+		const startPoint = this.position.startPoint;
+		const color = Color.createSchema(this.color);
+
+		let shape = new Path2D();
+		shape.rect(startPoint.x, startPoint.y, this.width, this.height);
+		$.fillStyle = color.fill;
+		$.fill(shape);
+		$.lineWidth = 10;
+		$.strokeStyle = color.stroke;
+		$.stroke(shape);
 	}
 }
