ab1af56c708213673a5a9970de447f849429ff05
mikoloism <mikoloism@gmail.com>
2023-04-01 18:19:10 +0330

    fix(shape): remove `position`, rename `types` on `shape` re-export pipe

--- src/shape/index.ts
+++ src/shape/index.ts
@@ -1,7 +1,6 @@
 export { default as Color } from './helpers/color';
-export { default as Position } from './helpers/position';
 export { default as Circle } from './shapes/circle';
 export { default as Rectangle } from './shapes/rectangle';
 export { default as Square } from './shapes/square';
 export { default as Triangle } from './shapes/triangle';
-export * from './type';
+export * from './types';
