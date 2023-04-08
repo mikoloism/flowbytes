b8260ab90b36400a9b2532b23dbe730f27459b51
mikoloism <mikoloism@gmail.com>
2023-04-08 21:11:23 +0330

    chore(config): configure `tsconfig.json` and `vite.config.ts` for project files path as `path-aliases` and `path-resolver`

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,37 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	resolve: {
 		alias: {
+			/** shape scope */
+			'shape::base': '/src/shape/index.ts',
+			'shape::circle': '/src/shape/shapes/circle.ts',
+			'shape::rectangle': '/src/shape/shapes/rectangle.ts',
+			'shape::square': '/src/shape/shapes/square.ts',
+			'shape::triangle': '/src/shape/shapes/triangle.ts',
+
+			/** event scope */
+			'events::click': '/src/dom-events/events/click.ts',
+			'events::keydown': '/src/dom-events/events/keydown.ts',
+			'events::keypress': '/src/dom-events/events/keypress.ts',
+			'events::enums': '/src/dom-events/helpers/keyboard.enum.ts',
+
+			/** vendors:operator scope */
 			'vendors/pipe': '/src/vendors/operator/pipe.ts',
 			'vendors/compose': '/src/vendors/operator/compose.ts',
-			'vendors/position': '/src/vendors/position.ts',
+			'vendors/task': '/src/vendors/operator/task.ts',
+			'vendors/chain': '/src/vendors/operator/chain.ts',
+
+			/** vendors scope  */
+			'vendors/position': '/src/vendors/position/index.ts',
+			'vendors/app': '/src/vendors/startup/index.ts',
+
+			/** vendors:storage scope */
+			'storage/base': '/src/vendors/storage/base.ts',
+			'storage/cache': '/src/vendors/storage/cache.ts',
+
+			/** src scope */
+			board: '/src/board',
+			shape: '/src/shape',
+			driver: '/src/driver',
 			'@': '/src',
 		},
 	},
