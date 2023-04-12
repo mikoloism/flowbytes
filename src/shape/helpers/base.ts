835219e1e6252f27d3a210be271077e36ba70164
mikoloism <mikoloism@gmail.com>
2023-04-12 11:46:07 +0330

    feat(shape): update `base` shape
    invoke auto-bind on `BaseShape`. add events methods by `on_` prefix as abstract protected methods.

--- src/shape/helpers/base.ts
+++ src/shape/helpers/base.ts
@@ -4,6 +4,7 @@ import type Position from 'vendors/position';
 import type { ShapeId } from '../types';
 import Color from './color';
 import { ShapeKind } from './kind';
+import withAutoBind from 'auto-bind';
 
 abstract class BaseShape {
 	public readonly id: ShapeId;
@@ -13,17 +14,30 @@ abstract class BaseShape {
 
 	public constructor() {
 		this.id = this.generateId();
+		withAutoBind(this);
 	}
 
 	protected generateId(): ShapeId {
 		return uuid.v4();
 	}
 
+	protected bound() {}
+
 	public setPosition(position: Position) {
 		this.position = position;
 	}
 
 	public abstract draw($: Svg): void | this;
+
+	protected abstract on_mouseleave(): void;
+	protected abstract on_mouseenter(): void;
+	protected abstract on_mousemove(): void;
+	protected abstract on_mousedown(): void;
+	protected abstract on_mouseup(): void;
+	protected abstract on_click(): void;
+	// protected abstract on_delete(): void;
+	// protected abstract on_mount(): void;
+	// protected abstract on_render(): void;
 }
 
 export default BaseShape;
